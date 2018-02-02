d46597c16e10d6d5fdd83fbf3c82ee3a6660ce5d
Tristan Escalada <tristan@escalada.us>
2018-02-02 17:16:28 -0500

    Add the ability to set arbitrary event.extra data
    Any data in run_status.sentry_extra will be merged into the event.extra field.

--- files/default/sentry.rb
+++ files/default/sentry.rb
@@ -59,6 +59,7 @@ class Chef::Handler::Sentry < Chef::Handler
       h[:policy_group] = node.policy_group if node.policy_group
       h[:policy_revision] = node['policy_revision'] if node['policy_revision']
     end
+    event.extra.merge!(run_status.sentry_extra)
     Raven.send_event(event)
   end
 end
